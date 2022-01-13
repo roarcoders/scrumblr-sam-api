39dfb3b6e4630e3024a8e23a146c8c6f46bf2731
Torey Littlefield <52614742+toreylittlefield@users.noreply.github.com>
2022-01-13 20:21:01 +0800

    [UPDATE]: onconnect to add connectionId to WS table

--- scrumblr-api/onconnect.js
+++ scrumblr-api/onconnect.js
@@ -1,30 +1,19 @@
 const AWS = require('aws-sdk');
-
-function getSocketContext(event) {
-  const { domainName, stage, connectionId } = event.requestContext;
-  const endpoint = `${domainName}/${stage}`;
-  const apigwManagementApi = new AWS.ApiGatewayManagementApi({
-    apiVersion: '2018-11-29',
-    endpoint,
-  });
-
-  // const send = async (data) => {
-  //   await apigwManagementApi.postToConnection({ ConnectionId: connectionId, Data: data }).promise();
-  // };
-
-  return { connectionId, endpoint };
-}
+const docClient = new AWS.DynamoDB.DocumentClient();
 
 module.exports.handler = async (event) => {
-  console.log(JSON.stringify(event, 2));
-
-  const { send } = getSocketContext(event);
-
-  const result = await send(JSON.stringify({ message: 'This response was push from my lambda' }));
-
-  return {
-    isBase64Encoded: false,
-    statusCode: 200,
-    body: '',
+  const putParams = {
+    TableName: process.env.TABLE_WEBSOCKET,
+    Item: {
+      ConnectionId: { S: event.requestContext.connectionId },
+    },
   };
+
+  try {
+    await docClient.put(putParams).promise();
+    return { statusCode: 200, body: 'Connected' };
+  } catch (error) {
+    console.error(error);
+    return { statusCode: 500, body: JSON.stringify(error) };
+  }
 };
