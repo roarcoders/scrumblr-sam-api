90c5b68e58836682fbb7ad9186801d09c5c09433
Zain <zain@192-168-1-2.tpgi.com.au>
2022-01-16 00:26:08 +1100

    Insert connectionId in dynamodb  upon successful client to  websocket connection

--- scrumblr-api/onconnect.js
+++ scrumblr-api/onconnect.js
@@ -2,18 +2,17 @@ const AWS = require('aws-sdk');
 const docClient = new AWS.DynamoDB.DocumentClient();
 
 module.exports.handler = async (event) => {
-  const putParams = {
+  const data = {
     TableName: process.env.TABLE_WEBSOCKET,
     Item: {
-      ConnectionId: { S: event.requestContext.connectionId },
+      ConnectionId: event.requestContext.connectionId,
     },
   };
 
   try {
-    await docClient.put(putParams).promise();
+    await docClient.put(data).promise();
     return { statusCode: 200, body: 'Connected' };
   } catch (error) {
-    console.error(error);
     return { statusCode: 500, body: JSON.stringify(error) };
   }
 };
