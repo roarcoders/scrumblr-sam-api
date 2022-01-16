346d585593fc40ed9df81b0001bad3b1cd079196
Torey Littlefield <52614742+toreylittlefield@users.noreply.github.com>
2022-01-16 19:55:50 +0800

    [update] delete connectionId from table

--- scrumblr-api/ondisconnect.js
+++ scrumblr-api/ondisconnect.js
@@ -1,30 +1,18 @@
 const AWS = require('aws-sdk');
-
-function getSocketContext(event) {
-  const { domainName, stage, connectionId } = event.requestContext;
-  const endpoint = `${domainName}/${stage}`;
-  const apigwManagementApi = new AWS.ApiGatewayManagementApi({
-    apiVersion: '2018-11-29',
-    endpoint,
-  });
-
-  // const send = async (data) => {
-  //   await apigwManagementApi.postToConnection({ ConnectionId: connectionId, Data: data }).promise();
-  // };
-
-  return { connectionId, endpoint };
-}
+const docClient = new AWS.DynamoDB.DocumentClient();
 
 module.exports.handler = async (event) => {
-  console.log(JSON.stringify(event, 2));
-
-  const { send } = getSocketContext(event);
-
-  //const result = await send(JSON.stringify({ message: 'This response was push from my lambda' }));
-
-  return {
-    isBase64Encoded: false,
-    statusCode: 200,
-    body: 'Connect',
+  const data = {
+    TableName: process.env.TABLE_WEBSOCKET,
+    Item: {
+      ConnectionId: event.requestContext.connectionId,
+    },
   };
+
+  try {
+    await docClient.delete(data).promise();
+    return { statusCode: 200, body: 'Disconnected' };
+  } catch (error) {
+    return { statusCode: 500, body: JSON.stringify(error) };
+  }
 };
